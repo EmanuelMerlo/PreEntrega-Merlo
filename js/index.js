4e51ec48b3d7e41ac872c001bc7b521681c30949
Emanuel Merlo <emanuelmerlo3@gmail.com>
2023-08-01 19:00:39 -0300

    pre entrega2 commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 // Bienvenida de Usuario
 
-
 alert("¡Bienvenido al sitio web de contratación de servicios para eventos!");
 
 let nombre = prompt("Por favor escriba su nombre.")
 let apellido = prompt("Por favor escriba su apellido.")
 
+class Persona {
+    constructor (name, lastName){
+        this.nombre = name;
+        this.apellido = lastName;
+    }
+}
+
+const persona1 = new Persona (nombre, apellido)
+
+console.log(persona1)
 
 while (nombre && apellido == " ") {
     alert("Ingresaste un nombre inválido")
@@ -19,20 +28,22 @@ alert("Bienvenido " + nombreUsuario)
 console.log("Nombre de cliente: " + nombreUsuario)
 alert("Obtendrá su presupuesto introduciendo los siguientes datos")
 
-//OBJETO USUARIO
-
-
-
 // METODO CONSTRUCTOR DE PRODUCTOS
 
-function Productos (nombre, precio) {
-    this.nombre = nombre
-    this.precio = precio
+class Producto {
+    constructor(nombre, precio){
+        this.nombre = nombre ;
+        this.precio = precio;
+    }
 }
 
 const cascadaDeChocolate = new Producto ("Cascada de chocolate", 2700)
 const catering = new Producto ("Catering", 1300)
 const mesaDulce = new Producto ("Mesa Dulce", 1650)
+
+const listaProductos = []
+
+listaProductos.push(cascadaDeChocolate, catering, mesaDulce)
 
 //funciones para realizar presupuesto
 
@@ -61,38 +72,39 @@ function presupuestar() {
     }
 }
 
-//Declaraciones de variables para el switch
+//Declaraciones de variables para el switch 
 
 let precioPorPersona = 0;
 let busqueda = prompt("Indique la opción con el número correspondiente: \n(1). Cascada de chocolate \n(2). Catering \n(3). Mesa dulce");
 
+
 while (isNaN(busqueda)){
-    alert("No contamos con ese servicio");
+    alert("No contamos con ese servicio")
     busqueda = prompt("Indique la opción con el número correspondiente: \n(1). Cascada de chocolate \n(2). Catering \n(3). Mesa dulce");
 }
 
 switch (busqueda) {
     case "1":
-        console.log("Elgiste el servicio de cascada de chocolate.")
-        alert("El precio por persona es de $2700.")
-        precioPorPersona = 2700
-        console.log("Precio por persona: $" + precioPorPersona)
+        console.log("Elgiste el servicio de " + cascadaDeChocolate.nombre )
+        alert("El precio por persona es de $" + cascadaDeChocolate.precio)
+        precioPorPersona = cascadaDeChocolate.precio
+        console.log("Precio por persona: $" + cascadaDeChocolate.precio)
         presupuestar()
         break;
 
     case "2":
-        console.log("Elegiste el servicio de catering.")
-        alert("El precio por persona es de $1300.")
-        precioPorPersona = 1300
-        console.log("Precio por persona: $" + precioPorPersona)
+        console.log("Elegiste el servicio de " + catering.nombre)
+        alert("El precio por persona es de $" + catering.precio)
+        precioPorPersona = catering.precio
+        console.log("Precio por persona: $" + catering.precio)
         presupuestar()
         break;
 
     case "3":
-        console.log("Elegiste el servicio de mesa dulce.")
-        alert("El precio por persona es de $1650")
-        precioPorPersona = 1650
-        console.log("Precio por persona: $" + precioPorPersona)
+        console.log("Elegiste el servicio de " + mesaDulce.nombre)
+        alert("El precio por persona es de $" + mesaDulce.precio)
+        precioPorPersona = mesaDulce.precio
+        console.log("Precio por persona: $" + mesaDulce.precio)
         presupuestar()
         break;
 
@@ -101,3 +113,28 @@ switch (busqueda) {
         console.log("¡No contrataste un servicio disponible!")
         break;
 }
+
+
+// Finalziación de presupuesto
+
+alert("Gracias por contratar nuestros servicios")
+
+
+// RECORRER ARRAY
+let conocer = prompt("Si quiere conocer la totalidad de nuestros servicios y su valor escriba si; de lo contrario escriba no.")
+
+switch (conocer) {
+    case "si":
+        listaProductos.forEach((i) => {console.log(i)} )
+        break;
+
+    case "no":
+        alert("Gracias por visitarnos")
+        break;
+
+    default:
+        alert("No ingresaste nada de lo que te pedimos")
+        break;
+}
+
+
